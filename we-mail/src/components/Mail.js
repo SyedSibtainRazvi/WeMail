12d524a141a5af1f8d8ebb6e8eb1d8d44e8805e6
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-02-16 20:06:49 +0530

    Update: Mail js

--- we-mail/src/components/Mail.js
+++ we-mail/src/components/Mail.js
@@ -12,6 +12,20 @@
 
 
 // function Mail() {
+  // const [mail, setMail] = useState([]);
+
+  // useEffect(() => {
+  //   var uriId = window.location.hash.split("#")[1];
+  //   fetch(
+  //       setMail(filteredData[0]);
+  //       console.log(filteredData);
+  //     });
+  // }, []);"https://run.mocky.io/v3/58770279-0738-4578-a1cf-c56a193fce98")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       var filteredData = data.filter((data) => {
+  //         return data.id === uriId;
+  //       });
   
 //   const history = useHistory();
 //   return (
@@ -60,7 +74,8 @@
 
 // export default Mail;
 
-import React, { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
+
 import "./Mail.css";
 import { IconButton } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
@@ -70,24 +85,10 @@ import EmailIcon from "@material-ui/icons/Email";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-import { useHistory } from "react-router-dom";
 
 function Mail() {
-  const [mail, setMail] = useState([]);
-
   const history = useHistory();
-  useEffect(() => {
-    var uriId = window.location.hash.split("#")[1];
-    fetch("https://run.mocky.io/v3/58770279-0738-4578-a1cf-c56a193fce98")
-      .then((response) => response.json())
-      .then((data) => {
-        var filteredData = data.filter((data) => {
-          return data.id === uriId;
-        });
-        setMail(filteredData[0]);
-        console.log(filteredData);
-      });
-  }, []);
+
   return (
     <div className="mail">
       <div className="mail__tools">
@@ -119,16 +120,15 @@ function Mail() {
           </IconButton>
         </div>
       </div>
-      {mail && (
         <div className="mail__body">
           <div className="mail__bodyHeader">
-            <h2>{mail.subject}</h2>
+            <h2>{"Hey, how are you"}</h2>
           </div>
           <div className="mail__message">
-            <p>{mail.body}</p>
+            <p>{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}</p>
           </div>
         </div>
-      )}
+      )
     </div>
   );
 }
